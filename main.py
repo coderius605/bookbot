8f0be3a07fded0794d86b93bee39ad349572bd0b
coderius605 <evanfingerstyle@gmail.com>
2024-08-01 16:24:21 -0700

    C: added count_characters and count_words functions

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    file_contents = get_book_text(book_path)
+    print(count_words(file_contents))
+
+    print(count_characters(file_contents))
+
+
+def count_words(string):
+    words = string.split()
+    return len(words)
+
+
+def count_characters(string):
+    lowered_string = string.lower()
+    words = lowered_string.split()
+    char_counts = {}
+
+    for word in words:
+        for character in word:
+            if character in char_counts:
+                char_counts[character] = char_counts[character] + 1
+            else:
+                char_counts[character] = 1
+    
+    return char_counts
+
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
